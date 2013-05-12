4724c4dd433dbd6377a582922489ba0810d339f6
Sheng-Loong Su <sushengloong@gmail.com>
2013-05-12 12:31:52 +0800

    user can submit new post

--- test/integration/posts_integration_test.rb
+++ test/integration/posts_integration_test.rb
@@ -6,4 +6,17 @@ class PostsIntegrationTest < ActionDispatch::IntegrationTest
     visit posts_path
     assert page.has_content? post.title
   end
+
+  test "should allow new post submission" do
+    visit new_post_path
+    title = 'Hello Kopitiam'
+    refute page.has_content?(title)
+    fill_in 'Title', with: title
+    fill_in 'Text', with: 'Kopi-O Jit Puay' 
+    fill_in 'Link URL', with: 'http://github.com'
+    click_button 'Submit'
+
+    assert posts_path, current_path
+    assert page.has_content?(title)
+  end
 end
