3fca0f15f26b72e49641a350e10da75d7c8e9a19
Sheng-Loong Su <sushengloong@gmail.com>
2013-05-12 12:59:09 +0800

    validate post creation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+
+  class EitherPresenceValidator < ActiveModel::Validator
+    def validate record
+      if options[:fields].blank? || !options[:fields].is_a?(Array) || options[:fields].length != 2
+        raise "#{self.class.to_s} must accept an array of two field symbols"
+      elsif options[:fields].all? { |field| record.send(field).blank? }
+        record.errors[:base] << "Either #{options[:fields].first} or #{options[:fields].last} must be present"
+      end
+    end
+  end
+
+  validates :title, presence: true
+  validates_with EitherPresenceValidator, fields: [:link, :text]
 end
--- test/integration/posts_integration_test.rb
+++ test/integration/posts_integration_test.rb
@@ -4,19 +4,29 @@ class PostsIntegrationTest < ActionDispatch::IntegrationTest
   test "should show all posts" do
     post = Post.create!(title: "Test Title", link: "rails.org", text: "Test text")
     visit posts_path
+
     assert page.has_content? post.title
   end
 
   test "should allow new post submission" do
+    before_count = Post.count
     visit new_post_path
-    title = 'Hello Kopitiam'
-    refute page.has_content?(title)
+    title = 'Hello Kopitiam Post 101'
     fill_in 'Title', with: title
     fill_in 'Text', with: 'Kopi-O Jit Puay' 
     fill_in 'Link URL', with: 'http://github.com'
     click_button 'Submit'
 
-    assert posts_path, current_path
     assert page.has_content?(title)
+    assert_equal before_count + 1, Post.count
+  end
+
+  test "should not create submission if form data is not valid" do
+    before_count = Post.count
+    visit new_post_path
+    click_button 'Submit'
+
+    assert page.has_content?('error')
+    assert_equal before_count, Post.count
   end
 end
